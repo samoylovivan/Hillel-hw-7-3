03c866c415d4bc6c41cb5edca6bb9ca355623320
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2019-07-25 16:59:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,15 +10,34 @@ public class Main {
         String dateStr = new String("22.01.2019 19:15:00");
         SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 
-        try {
-            Date date = formatDate.parse(dateStr);
-            dateStr = new SimpleDateFormat("d MMM', ' h:mm a", Locale.ENGLISH).format(date);
+        if (isValidDate(dateStr, "dd.MM.yyyy HH:mm:ss")) {
+            try {
+                Date date = formatDate.parse(dateStr);
+                dateStr = new SimpleDateFormat("d MMM', ' h:mm a", Locale.ENGLISH).format(date);
+            } catch (Exception error) {
+                System.out.println(error);
+            }
+            System.out.println(dateStr);
         }
-        catch (Exception error){
-            System.out.println(error);
+        else{
+            System.out.println("Date is not valid.");
         }
-
-        System.out.println(dateStr);
     }
 
+    static boolean isValidDate(String value, String datePattern) {
+
+        if (value == null || datePattern == null || datePattern.length() <= 0) {
+            return false;
+        }
+
+        SimpleDateFormat formatter = new SimpleDateFormat(datePattern);
+        formatter.setLenient(false);
+
+        try {
+            formatter.parse(value);
+        } catch (Exception error) {
+            return false;
+        }
+        return true;
+    }
 }
